6d4f61d8ac7e46359eac46c040e4e26bfa2cd697
Raul Burbano <raulbi_92@hotmail.com>
2020-08-14 18:54:13 -0500

    crifado de contrasena

--- usuarios/usuario.py
+++ usuarios/usuario.py
@@ -21,7 +21,7 @@ class Usuario:
     def __init__(self, nombre, apellido, email, password):
         self.nombre= nombre
         self.apellido= apellido
-        self.email= email
+        self.email= email  
         self.password= password
     
     def registrar(self):
