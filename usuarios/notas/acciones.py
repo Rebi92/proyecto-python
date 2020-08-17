c66d4804ca9148d74178eedcfed139abe50eb924
Raul Burbano <raulbi_92@hotmail.com>
2020-08-17 12:03:41 -0500

    función de crear notas

--- usuarios/notas/acciones.py
+++ usuarios/notas/acciones.py
@@ -1,7 +1,18 @@
+import notas.nota as modelo
+
 class Acciones:
 
     def crear(self, usuario):
-        print(f"Ok {usuario[1]}, vamos a crear una nueva nota...")
-        
-    
+        print(f"\nOk {usuario[1]}, vamos a crear una nueva nota...")
+
+        titulo= input("\nIntriduce el numero de tu nota: ")
+        descripcion= input("Mete el contenido de tu nota")
+
+        nota= modelo.Nota(usuario[0], titulo, descripcion)
+        guardar= nota.guardar()
+
+        if guardar[0] >= 1:
+            print(f'\nPerfecto has guardado la nota {nota.titulo}')  
+        else:
+            print(f"No se a guardado la nota {usuario[1]}")  
     
